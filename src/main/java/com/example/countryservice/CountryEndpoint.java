f2905b69cf8a993490221b7b8f424d0576723b8e
Sudip Bhattacharya <sudipbh@gmail.com>
2021-02-19 12:29:24 -0500

    Initial commit

---
+++ src/main/java/com/example/countryservice/CountryEndpoint.java
@@ -0,0 +1,33 @@
+package com.example.countryservice;
+
+import com.kpmg.sudip.country_web_service.GetCountryRequest;
+import com.kpmg.sudip.country_web_service.GetCountryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CountryEndpoint {
+
+	public static final String NAMESPACE_URI = "http://kpmg.com/sudip/country-web-service";
+	public static final String PORTTYPE_NAME = "CountriesPort";
+	public static final String LOCATION_URL = "/ws";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+		return response;
+	}
+
+}
